7ba823be4f7ea1219f41bee902f436fe007c55ab
PreethiVignesh17 <167072514+PreethiVignesh17@users.noreply.github.com>
2024-09-03 15:10:45 +0530

    Add files via upload

---
+++ Demo.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class Demo {
+
+	public static void test() {
+		System.out.println("world");
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+		test();
+	}
+	
+
+
+}
---
+++ TEstng.java
@@ -0,0 +1,21 @@
+package org.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.github.dockerjava.api.model.Driver;
+
+import io.cucumber.java.en.Given;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class TEstng {
+	WebDriver driver;
+	
+	public void test1() {
+		
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+	}
+
+}
